8ee6ca01bd6158162ce9cf7ff946c788f18a9c2c
Eric Pashman <eric.pashman@gmail.com>
2014-09-12 17:40:34 -0500

    Update Cabal for AD 4, with stripped-down, half-working Lagrangian.Internal

--- src/Numeric/AD/Lagrangian/Internal.hs
+++ src/Numeric/AD/Lagrangian/Internal.hs
@@ -1,46 +1,30 @@
 {-# LANGUAGE Rank2Types, FlexibleContexts #-}
+
 module Numeric.AD.Lagrangian.Internal where
+
+import Numeric.AD
+import Numeric.AD.Internal.Reverse
+
 import Numeric.Optimization.Algorithms.HagerZhang05
+-- import Numeric.LinearAlgebra.Algorithms
+
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Storable as S
-import Numeric.AD
-import GHC.IO                   (unsafePerformIO)
-import Numeric.AD.Types
-import Numeric.AD.Internal.Classes
-import Numeric.LinearAlgebra.Algorithms
 import qualified Data.Packed.Vector as V
-import qualified Data.Packed.Matrix as M
-import Numeric.AD.Internal.Tower
+-- import qualified Data.Packed.Matrix as M
 
-infixr 1 <=>
--- | Build a 'Constraint' from a function and a constant
-(<=>) :: (forall s r. (Mode s, Mode r) => [AD2 s r a] -> AD2 s r a) -> a -> Constraint a
-g <=> c = (FU g,c)
+import Data.Reflection
 
--- | A constraint of the form @g(x, y, ...) = c@. See '<=>' for constructing a 'Constraint'.
-type Constraint a = (FU a, a)
+import GHC.IO (unsafePerformIO)
 
-type AD2 s r a = AD s (AD r a)
+--------------------------------------------------------------------------------
+-- The goal is to abstract `minimize` over `toMin` and `constraints`. ...
+--------------------------------------------------------------------------------
 
--- | A newtype wrapper for working with the rank 2 types constraint functions.
-newtype FU a = FU {unFU :: forall s r. (Mode s, Mode r) => [AD2 s r a] -> AD2 s r a}
-
--- | This is the lagrangian multiplier solver. It is assumed that the
---   objective function and all of the constraints take in the
---   same amount of arguments.
 minimize :: Double
-      -> (forall s r. (Mode s, Mode r) => [AD2 s r Double] -> AD2 s r Double)
-        -- ^ The function to minimize
-      -> [Constraint Double]
-      -- ^ The constraints as pairs @g \<=\> c@ which represent equations
-      --   of the form @g(x, y, ...) = c@
-      -> Int
-      -- ^ The arity of the objective function which should equal the arity of
-      --   the constraints.
-      -> Either (Result, Statistics) (S.Vector Double, S.Vector Double)
-      -- ^ Either an explanation of why the gradient descent failed or a pair
-      --   containing the arguments at the minimum and the lagrange multipliers
-minimize tolerance toMin constraints argCount = result where
+         -> Int
+         -> Either (Result, Statistics) (V.Vector Double, V.Vector Double)
+minimize tolerance {-toMin constraints-} argCount = result where
     -- The function to minimize for the langrangian is the squared gradient
     obj argsAndLams =
         squaredGrad (lagrangian toMin constraints argCount) argsAndLams
@@ -50,70 +34,75 @@ minimize tolerance toMin constraints argCount = result where
     -- perhaps this should be exposed
     guess = U.replicate (argCount + constraintCount) (1.0 :: Double)
 
+    --result = undefined
     result = case unsafePerformIO $
                     optimize
-                        (defaultParameters { printFinal = False })
+                        (defaultParameters {
+                            printFinal = False
+                          , verbose = VeryVerbose  -- For debugging ...
+                          })
                         tolerance
                         guess
-                        (VFunction (lowerFU obj . U.toList))
+                        (VFunction (obj . U.toList))
                         (VGradient (U.fromList . grad obj . U.toList))
                         Nothing of
        (vs, ToleranceStatisfied, _) -> Right (S.take argCount vs,
                                               S.drop argCount vs)
        (_, x, y) -> Left (x, y)
 
--- | Finding the maximum is the same as the minimum with the objective function inverted
-maximize :: Double
-      -> (forall s r. (Mode s, Mode r) => [AD2 s r Double] -> AD2 s r Double)
-        -- ^ The function to maximize
-      -> [Constraint Double]
-      -- ^ The constraints as pairs @g \<=\> c@ which represent equations
-      --   of the form @g(x, y, ...) = c@
-      -> Int
-      -- ^ The arity of the objective function which should equal the arity of
-      --   the constraints.
-      -> Either (Result, Statistics) (S.Vector Double, S.Vector Double)
-      -- ^ Either an explanation of why the gradient descent failed or a pair
-      --   containing the arguments at the minimum and the lagrange multipliers
-maximize tolerance toMax constraints argCount =
-    minimize tolerance (negate1 . toMax) constraints argCount
 
-lagrangian :: (Num a, Mode s, Mode r)
-           => (forall s r. (Mode s, Mode r) => [AD2 s r a] -> AD2 s r a)
-           -> [Constraint a]
-           -> Int
-           -> [AD2 s r a]
-           -> AD2 s r a
+lagrangian :: (Num a) => ([a] -> a) -> [([a] -> a, a)] -> Int -> [a] -> a
 lagrangian f constraints argCount argsAndLams = result where
     args = take argCount argsAndLams
     lams = drop argCount argsAndLams
 
     -- g(x, y, ...) = c <=> g(x, y, ...) - c = 0
-    appliedConstraints = fmap (\(FU f, c) -> f args - (auto . auto) c) constraints
+    appliedConstraints = fmap (\(g, c) -> g args - (c)) constraints
 
     -- L(x, y, ..., lam0, ...) = f(x, y, ...) + lam0 * (g0 - c0) ...
-    result = f args + (sum . zipWith (*) lams $ appliedConstraints)
+    result = (f args) + (sum . zipWith (*) lams $ appliedConstraints)
 
-squaredGrad :: Num a
-            => (forall s. Mode s => [AD s a] -> AD s a)
+
+squaredGrad :: (Num a)
+            => (forall s. (Reifies s Tape) => [Reverse s a] -> Reverse s a)
             -> [a] -> a
-squaredGrad f vs = sum . fmap (^2) . grad f $ vs
+squaredGrad f = sum . fmap square . grad f where
+    square x = x * x
 
--- | WARNING. Experimental.
---   This is not a true feasibility test for the function. I am not sure
---   exactly how to implement that. This just checks the feasiblility at a point.
---   If this ever returns false, 'solve' can fail.
-feasible :: (forall s r. (Mode s, Mode r) => [AD2 s r Double] -> AD2 s r Double)
-         -> [Constraint Double]
-         -> [Double]
-         -> Bool
-feasible toMin constraints points = result where
-    obj argsAndLams =
-        squaredGrad (lagrangian toMin constraints $ length points) argsAndLams
+--------------------------------------------------------------------------------
+-- Run, e.g., `minimize (1e-10) 4` to verify that it works when closing over the
+-- example objective function and constraints. 
+--------------------------------------------------------------------------------
 
-    hessianMatrix = M.fromLists . hessian obj $ points
+-- Right now, `minimize` closes over this ...
+toMin :: (Floating a) => [a] -> a
+toMin = entropy
 
-    -- make sure all of the eigenvalues are positive
-    result = all (>0) . V.toList . eigenvaluesSH $ hessianMatrix
+-- ... and this.
+constraints :: (Num a, Num b) => [([a] -> a, b)]
+constraints = [probSum]
 
+entropy :: (Floating a) => [a] -> a
+entropy = sum . fmap (\x -> x * log x)
 
+probSum :: (Num a, Num b) => ([a] -> a, b)
+probSum = (sum, 1)
+
+--------------------------------------------------------------------------------
+-- Temporary stubs for code imported by Numeric.AD.Lagrangian
+--------------------------------------------------------------------------------
+
+data Constraint = Constraint
+
+data FU = FU
+
+data AD2 = AD2
+
+(<=>) :: a
+(<=>) = undefined
+
+maximize :: a
+maximize = undefined
+
+feasible :: a
+feasible = undefined
